673d741cc0e38b725074916a4d1fb0150b460d69
Moises Ribeiro <moisesrms@gmail.com>
2019-09-16 20:52:31 -0300

    [#3] remove unnecessary http server creation

--- server.js
+++ server.js
@@ -5,17 +5,7 @@ const typeDefs = require('./graphql/schema')
 const resolvers = require('./graphql/resolvers')
 const fs = require('fs')
 const https = require('https')
-const http = require('http')
 require('dotenv').config()
-
-const configurations = {
-  // Note: You may need sudo to run on port 443
-  production: { ssl: true, port: 443, hostname: 'localhost' },
-  development: { ssl: false, port: 4000, hostname: 'localhost' }
-}
-
-const environment = process.env.NODE_ENV || 'production'
-const config = configurations[environment]
 
 const apollo = new ApolloServer({
   typeDefs,
@@ -27,28 +17,19 @@ const apollo = new ApolloServer({
 const app = express()
 apollo.applyMiddleware({ app })
 
-// Create the HTTPS or HTTP server, per configuration
-var server
-if (config.ssl) {
-  // Assumes certificates are in .ssl folder from package root. Make sure the files
-  // are secured.
-  server = https.createServer(
-    {
-      key: fs.readFileSync('ssl/server.key'),
-      cert: fs.readFileSync('./ssl/server.cert')
-    },
-    app
-  )
-} else {
-  server = http.createServer(app)
-}
+var server = https.createServer(
+  {
+    key: fs.readFileSync('ssl/server.key'),
+    cert: fs.readFileSync('./ssl/server.cert')
+  },
+  app
+)
 
-// Add subscription support
 apollo.installSubscriptionHandlers(server)
 
-server.listen({ port: config.port }, () =>
+server.listen({ port: process.env.PORT }, () =>
   console.log(
     'Server ready at',
-    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${apollo.graphqlPath}`
+    `https://${process.env.HOSTNAME}${apollo.graphqlPath}`
   )
 )
